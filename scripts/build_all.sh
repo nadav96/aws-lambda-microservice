acbd4c611df4fd966a0b64b2136386b1a50db56a
Nadav Goldstein <nadav.goldstein96@gmail.com>
2018-09-13 16:20:10 +0300

    Fixed the build all script to pass the app name to the hash function instead of hardcoded value. Also made it use the hash function only if the build folder exists, it will build it if not exists.

--- scripts/build_all.sh
+++ scripts/build_all.sh
@@ -10,10 +10,16 @@ do
         let "total_apps_count=total_apps_count+1"
 
         # Fetch the previous hash if exists
-        previous_hash=$(cat $(pwd)/build/demo/function_dir_hash)
-        # Compute the current hash of the entire function folder
-        python $(pwd)/scripts/hash_function.py demo
-        current_hash=$(cat $(pwd)/build/demo/function_dir_hash)
+        if [  -f  $(pwd)/build/${app_name}/function_dir_hash ]; then
+            previous_hash=$(cat $(pwd)/build/${app_name}/function_dir_hash)
+            # Compute the current hash of the entire function folder
+            python $(pwd)/scripts/hash_function.py ${app_name}
+            current_hash=$(cat $(pwd)/build/${app_name}/function_dir_hash)
+        else
+            previous_hash="0"
+            current_hash="1"
+        fi
+
 
         if [ "$previous_hash" != "$current_hash" ]; then
             # The function dir hash was changed, rebuild the function, and compute the hash again.
@@ -24,7 +30,7 @@ do
             ./scripts/build_function.sh ${app_name}
 
             # Compute the hash again after the build
-            python $(pwd)/scripts/hash_function.py demo
+            python $(pwd)/scripts/hash_function.py ${app_name}
         fi
     fi
 done
